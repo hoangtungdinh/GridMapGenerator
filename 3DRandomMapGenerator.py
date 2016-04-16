f42c44ed949a6f8e236e5c2f43a217df145b0a9b
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-04-16 22:45:51 +0200

    improved performance of 3D map generator

--- 3DRandomMapGenerator.py
+++ 3DRandomMapGenerator.py
@@ -2,18 +2,11 @@ import random
 from WriteMapToFile import write_3d_map_to_file
 
 
-def generate_map(depth, percentage_occupied_cells, seed):
+def generate_map(depth, percentage_occupied_cells, seed, all_pos):
     width = 2 ** depth
     num_of_objects = int(width ** 3 * percentage_occupied_cells / 100)
+
     random.seed(seed)
-
-    all_pos = set()
-
-    for x in range(width):
-        for y in range(width):
-            for z in range(width):
-                all_pos.add((x, y, z))
-
     object_pos = random.sample(all_pos, num_of_objects)
 
     map = generate_free_map(depth)
@@ -39,12 +32,19 @@ def main():
     seed_list = random.sample(range(1, 1000000), 180)
     percentage = [5, 10, 15, 20, 25, 30]
 
+    width = 2 ** depth
+    all_pos = set()
+    for x in range(width):
+        for y in range(width):
+            for z in range(width):
+                all_pos.add((x, y, z))
+
     for percent in percentage:
         for i in range(30):
             seed = seed_list.pop(0)
             file_name = 'map/map' + str(percent) + 'percent' + str(i) + '.txt'
-            write_3d_map_to_file(generate_map(depth, percent, seed), resolution, file_name)
-            print("Done " + file_name + "\n")
+            write_3d_map_to_file(generate_map(depth, percent, seed, all_pos), resolution, file_name)
+            print("Done " + file_name)
 
 
 if __name__ == '__main__':
